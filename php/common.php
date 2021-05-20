b308dea5c4f2d6a24ae4efe2d2bedb274c5d358d
hermes3306 <joonho.park@hotmail.com>
2021-05-21 00:18:26 +0900

    x

---
+++ php/common.php
@@ -0,0 +1,8 @@
+<?php
+    $moment_home = "/home/pi/code/MomentCloud";
+    $dbfile      = "/home/pi/code/MomentCloud/Mpment.db";
+    $moment_url  = "http://ezehub.club/moment";
+
+//	error_reporting( E_ALL );
+//	ini_set( "display_errors", 1 );
+?>
---
+++ php/file_list.php
@@ -0,0 +1,21 @@
+<?php include('common.php'); ?>
+<?php
+	$dir = $_GET['dir'];
+	$ext = $_GET['ext'];
+
+	if( is_null($dir)) $dir = ".";
+	if( is_null($ext)) $ext = "jpeg";
+
+	$path = $moment_home . '/' . $dir;
+	$files = glob($path . "/*." . $ext, GLOB_BRACE);
+	$listfiles=array();
+	$thelist ="";
+	foreach($files as $f){
+          if ($f != "." && $f != "..") {
+            // $thelist = $thelist.$f.'<br>';
+			$fname = basename($f);
+            $thelist = $thelist. $fname. '<br>';
+          }
+	}
+	echo $thelist;
+?>
